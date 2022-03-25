9df9716b1b19507d8d81efe5f912daa2e780c649
katzedaze <fairycat.caitsith@gmail.com>
2022-03-25 23:13:22 +0900

    test_commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,46 @@
-import { useState } from "react";
-import "./styles.css";
-import { InputTodo } from "./components/InputTodo";
-import { IncompleteTodos } from "./components/IncompleteTodos";
-import { CompleteTodos } from "./components/CompleteTodos";
+import { useState } from 'react'
+import { CompleteTodos } from './components/CompleteTodos'
+import { IncompleteTodos } from './components/IncompleteTodos'
+import { InputTodo } from './components/InputTodo'
+import './styles.css'
 
 export const App = () => {
-  const [todoText, setTodoText] = useState("");
-  const [incompleteTodos, setIncompleteTodos] = useState([]);
-  const [completeTodos, setCompleteTodos] = useState([]);
+  const [todoText, setTodoText] = useState('')
+  const [incompleteTodos, setIncompleteTodos] = useState([])
+  const [completeTodos, setCompleteTodos] = useState([])
 
-  const onChangeTodoText = (event) => setTodoText(event.target.value);
+  const onChangeTodoText = (event) => setTodoText(event.target.value)
 
   const onClickAdd = () => {
-    if (todoText === "") return;
-    const newTodos = [...incompleteTodos, todoText];
-    setIncompleteTodos(newTodos);
-    setTodoText("");
-  };
+    if (todoText === '') return
+    const newTodos = [...incompleteTodos, todoText]
+    setIncompleteTodos(newTodos)
+    setTodoText('')
+  }
 
   const onClickDelete = (index) => {
-    const newTodos = [...incompleteTodos];
-    newTodos.splice(index, 1);
-    setIncompleteTodos(newTodos);
-  };
+    const newTodos = [...incompleteTodos]
+    newTodos.splice(index, 1)
+    setIncompleteTodos(newTodos)
+  }
 
   const onClickComplete = (index) => {
-    const newIncompleteTodos = [...incompleteTodos];
-    newIncompleteTodos.splice(index, 1);
+    const newIncompleteTodos = [...incompleteTodos]
+    newIncompleteTodos.splice(index, 1)
 
-    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
-    setIncompleteTodos(newIncompleteTodos);
-    setCompleteTodos(newCompleteTodos);
-  };
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]]
+    setIncompleteTodos(newIncompleteTodos)
+    setCompleteTodos(newCompleteTodos)
+  }
 
   const onClickBack = (index) => {
-    const newCompleteTodos = [...completeTodos];
-    newCompleteTodos.splice(index, 1);
+    const newCompleteTodos = [...completeTodos]
+    newCompleteTodos.splice(index, 1)
 
-    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
-    setCompleteTodos(newCompleteTodos);
-    setIncompleteTodos(newIncompleteTodos);
-  };
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]]
+    setCompleteTodos(newCompleteTodos)
+    setIncompleteTodos(newIncompleteTodos)
+  }
 
   return (
     <>
@@ -51,9 +51,7 @@ export const App = () => {
         disabled={incompleteTodos.length >= 8}
       />
       {incompleteTodos.length >= 8 && (
-        <p style={{ color: "red" }}>
-          登録できるtodoは8個までです。タスクを消化してください。
-        </p>
+        <p style={{ color: 'red' }}>登録できるtodoは8個までです。タスクを消化してください。 test</p>
       )}
       <IncompleteTodos
         todos={incompleteTodos}
@@ -62,5 +60,5 @@ export const App = () => {
       />
       <CompleteTodos todos={completeTodos} onClickBack={onClickBack} />
     </>
-  );
-};
+  )
+}
